013a1a9599ad8b60ca6970251235e18cf0249a3b
dotnotation <dotinmotionproductions@gmail.com>
2021-11-10 11:55:35 -0500

    commit tree f2bd659bbf25fb476d462fbd69f701223fdee879

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const driver = {}; 
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    // does not mutate driver and return a new driver with an updated value and key
+    return Object.assign({}, driver, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    // mutate the driver
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key){
+    // delete the key/value pair but not mutate the driver
+    const newDriver = Object.assign({}, driver);
+    delete newDriver[key];
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]; 
+    return driver;
+}
